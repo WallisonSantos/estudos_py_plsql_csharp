c000994d5af26dc20d7ddb76d30e4e90a8ba0507
Wallison Santos <wallisontiagoassis206@outlook.com>
2022-06-29 05:15:42 -0300

    -

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+
+*/
+using System;
 namespace csharp
 {
     class Program
@@ -8,7 +11,17 @@ namespace csharp
             Console.WriteLine("Hello World !");
             Console.WriteLine("Bom dia !");
             Console.ReadLine();
-            
+
+            int i;
+            int i2 = i = 2;
+            Console.WriteLine("i2 " + i2);
+            Console.WriteLine("i " + i);
+
+            i = i2 == 42 ?  10  :  20;
+            Console.WriteLine(i);
+
+
+
         }
     }
 }
