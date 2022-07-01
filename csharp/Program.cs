6ff4fb6b86ba4c642681486252acd341887c9922
Wallison Santos <wallisontiagoassis206@outlook.com>
2022-07-01 02:04:13 -0300

    commit

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,20 +8,31 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
-            Console.WriteLine("Bom dia !");
-            Console.ReadLine();
+            char gen  = '\u04b3';
+            string str = "Maria Green";
+            object obj1 = "alex brown";
+            object obj2 = 5.5f;
 
-            int i;
-            int i2 = i = 2;
-            Console.WriteLine("i2 " + i2);
-            Console.WriteLine("i " + i);
+            sbyte x = -119 ;
+            byte y = 255 ;
 
-            i = i2 == 42 ?  10  :  20;
-            Console.WriteLine(i);
+            int num1  = 2147483647 ;
+            long num2 = 2147483123999L ;
+            double num3 = 4.5 ;
+            float num4 = 4.5f ;
 
+            y ++ ;
+            x ++ ;
+            
+            System.Console.WriteLine("Valor para " + gen) ;
 
+            Console.WriteLine("Valor para Sbyte " + x ) ;
+            Console.WriteLine("Valor para byte " + y ) ;
 
+            Console.WriteLine("Valor para int " + num1) ;
+            Console.WriteLine("Valor para long " + num2) ;
+            Console.WriteLine("Valor para double " + num3) ;
+            Console.WriteLine("Valor para float " + num4) ;
         }
     }
 }
