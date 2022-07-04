43da93dc85ea69556a303de84bfd601e686bf5d3
Wallison Santos <wallisontiagoassis206@outlook.com>
2022-07-03 23:02:46 -0300

    Estudos, Python, Csharp e PLSQL

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,44 +1,21 @@
-﻿/*
-
-*/
-using System;
+﻿using System;
+using System.Globalization;
 namespace csharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            char gen  = '\u04b3';
-            string str = "Maria Green";
-            object obj1 = "alex brown";
-            object obj2 = 5.5f;
-            
+            /*
+                Interpolação de strings
+            */
+            int idade = 32;
+            double saldo = 10.8920;
+            string nome = "Maria";
 
-            sbyte x = -119 ;
-            byte y = 255 ;
-
-            int num1  = 2147483647 ;
-            long num2 = 2147483123999L ;
-            double num3 = 4.5 ;
-            float num4 = 4.5f ;
-
-            y ++ ;
-            x ++ ;
-            
-
-            System.Console.WriteLine("Valor para " + gen) ;
-
-            System.Console.WriteLine("Valor para Sbyte " + x ) ;
-            System.Console.WriteLine("Valor para byte " + y ) ;
-
-            System.Console.WriteLine("Valor para int " + num1) ;
-            System.Console.WriteLine("Valor para long " + num2) ;
-            System.Console.WriteLine("Valor para double " + num3) ;
-            System.Console.WriteLine("Valor para float " + num4) ;
-
-            System.Console.WriteLine("Valor para " + str);
-            
-
+            System.Console.WriteLine("A {0} tem {1} anos e saldo em conta de {2:F2} reais ", nome, idade, saldo);
+            System.Console.WriteLine($"A {nome} tem {idade} anos e saldo em conta de {saldo:F2} reais ");
+            System.Console.WriteLine("A " + nome + " tem " + idade + " anos e saldo em conta de " + saldo.ToString());
         }
     }
 }
