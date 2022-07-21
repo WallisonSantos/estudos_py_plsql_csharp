4f026d4cdc09033f5d3a1cf2ad1efcc98b7cf983
Wallison Santos <wallisontiagoassis206@outlook.com>
2022-07-20 22:22:09 -0300

    Commit

--- java-project/src/App.java
+++ java-project/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,38 +12,39 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Fazer uma conexão HTTP e buscar os top 250 filmes:
-        String url   = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String url   = "https://raw.githubusercontent.com/alexfelipe/imersao-java/json/top250.json";
         
-
         URI endereco = URI.create(url);
         var client   = HttpClient.newHttpClient();
         var request  = HttpRequest.newBuilder(endereco).GET().build();
 
-
         // Utilizar of String pois o send precisa saber que queremos parsear um object do tipo String
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         
-
         String body  = response.body();
-
 
         // extrair só os dados que interessam (título, poster, classificação)
         var parser   = new JsonParser();
-        List<Map<String, String>> listaDeFlmes = parser.parse(body);
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        System.out.println(listaDeFlmes.size());
-        System.out.println(listaDeFlmes.get(0));
-
+        System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes.get(0));
 
         // exibir e manipular os dados 
-        for (Map<String,String> filme : listaDeFlmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imdbRating"));
-            System.out.println("\n");
+        for (Map<String,String> filme : listaDeFilmes) {
+            
+            String urlImg   = filme.get("image");
+            String title    = filme.get("title");
+            String nomeFile = title + ".png";
+
+            InputStream inputStream = new URL(urlImg).openStream();
+
+            var geraFig = new GeradorDeFiguras();
+
+            geraFig.criaFiguras(inputStream, nomeFile);;
+
+            System.out.println(title);
+            System.out.println();
         }
-
-
-
     }
 }
--- java-project/src/GeradorDeFiguras.java
+++ java-project/src/GeradorDeFiguras.java
@@ -1,42 +1,44 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
+
 import javax.imageio.ImageIO;
 
 public class GeradorDeFiguras {
     
-    public void criaFiguras() throws Exception {
+    public void criaFiguras(InputStream inputStream, String nomeFile) throws Exception {
 
         // Leitura da Imagem
-        BufferedImage imagemGalax = ImageIO.read(new File("java-project/galax.jpg"));
+        // InputStream inputStream = new URL("https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png").openStream();
+        BufferedImage imagemGalax = ImageIO.read(inputStream);
 
 
         // Cria nova imagem em memória com transparencia e com tamanho novo
         int largura = imagemGalax.getWidth();
         int altura = imagemGalax.getHeight();
-        int novaAltura = altura + 200;
-        BufferedImage novaImagemGalax = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+        int novaAltura = altura + 80;
+        BufferedImage vectorImagemGalax = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
-
+        
         // Copiar a imagem original original para nova imagem (em memória)
-        Graphics2D graphics =  (Graphics2D) novaImagemGalax.getGraphics();
+        Graphics2D graphics =  (Graphics2D) vectorImagemGalax.getGraphics();
         graphics.drawImage(imagemGalax, 0, 0, null);
         
-        // Escrever uma frase na nova imagem 
 
+        // Configurar a Fonte
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+        graphics.setFont(font);
+        graphics.setColor(Color.BLUE);
+   
+
+        // Escrever uma frase na nova imagem 
+        graphics.drawString(" Google ", 50, novaAltura - 30);
 
 
         // Escrever a nova imagem em um arquivo
-        ImageIO.write(novaImagemGalax, "png", new File("java-project/newgalax.png"));
-
-
-        
+        ImageIO.write(vectorImagemGalax, "png", new File(nomeFile));
     }
-
-    public static void main(String[] args) throws Exception {
-        var GerFig = new GeradorDeFiguras();
-        GerFig.criaFiguras();
-
-    }
-
 }
